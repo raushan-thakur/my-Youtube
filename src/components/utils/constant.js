eaa4e05366fd5944f205a8de51a3e7f786693128
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2024-03-30 17:23:39 +0530

    Update constant.js

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -10,4 +10,4 @@ export const YOUTUBE_VIDEOS_API =
 //   "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=YOURKEYWORD&type=video&key=" +
 //   GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API ="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API ="https://proxy.cors.sh/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
